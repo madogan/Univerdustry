0436e92cbe0824abfd401cfa30e9c1aa8fd7e0b9
madogan <muhammed.ali.dogan@outlook.com>
2019-12-25 20:53:41 +0300

    Trivial.

--- gateway/app.py
+++ gateway/app.py
@@ -22,8 +22,7 @@ logger.add(sink=sys.stderr,
 
 # Import necessary libraries.
 import requests
-from flask import Flask, jsonify
-
+from flask import Flask, jsonify, request, abort, render_template
 
 # Create application instance.
 app = Flask(__name__)
@@ -33,17 +32,22 @@ from custom_json_encoder import CustomJsonEncoder
 
 app.json_encoder = CustomJsonEncoder
 
+ELASTICSEARCH_URL = os.getenv("ELASTICSEARCH_URL")
+DATABASE_REST_URL = os.getenv("DATABASE_REST_URL")
+
 from elasticsearch import Elasticsearch
-es = Elasticsearch(os.getenv("ELASTICSEARCH_URL"))
+
+es = Elasticsearch(ELASTICSEARCH_URL)
 
 
 @app.route("/")
 @app.route("/index")
-def index(path):
-    return "Gateway is working..."
+def index():
+    return render_template("index.html")
 
 
-@app.route(f'/api/v{__version__}/scrape/<organization_domain>', methods=["GET"])
+@app.route(f'/api/v{__version__}/scrape/<organization_domain>',
+           methods=["GET"])
 def scrape(organization_domain):
     url = os.getenv("SCRAPER_URL") + "/univerdustry/scraper/start"
 
@@ -56,30 +60,46 @@ def scrape(organization_domain):
         return jsonify({"status": "error", "message": response["message"]}), \
                500
 
-#
-# @app.route(f'/api/v{__version__}/search', methods=["GET"])
-# def search():
-#     data = request.args.json()
-#
-#     if "query" not in data:
-#         abort(404)
-#
-#     result = es.search(index="marmara_edu_tr",
-#                        body={"query": {"match": {"full_text": data["query"]}}})
-#
-#     return jsonify(result)
-#
-#
-# @app.route(f'/api/v{__version__}/author/<author_id>', methods=["GET"])
-# def get_author(author_id):
-#     # TODO: Input check.
-#     return jsonify(requests.get(DATABASE_REST_URL + f'/author?ident=eq.{author_id}'))
-#
-#
-# @app.route(f'/api/v{__version__}/publication/<publication_id>', methods=["GET"])
-# def get_publication(publication_id):
-#     # TODO: Input check.
-#     return jsonify(requests.get(DATABASE_REST_URL + f'/publication?ident=eq.{publication_id}'))
-#
-#
-#
+
+@app.route(f'/api/v{__version__}/search', methods=["GET"])
+def search():
+    data = request.args
+
+    if "q" not in data:
+        abort(404)
+
+    try:
+        es_data = es.search(index="_all", body={
+            "query": {
+                "multi_match": {
+                    "query": data["q"],
+                    "fields": ["publication.content_text",
+                               "publication.title", "author.affiliation",
+                               "author.name", "author.interests",
+                               "publication.ident", "author.ident"]
+                }
+            }
+        })
+        result = {"status": "ok",
+                  "data": es_data.get("hits", {}).get("hits", [])}
+    except Exception as e:
+        result = {"status": "failure", "message": str(e)}
+
+    return jsonify(result)
+
+
+@app.route(f'/api/v{__version__}/author/<author_id>', methods=["GET"])
+def get_author(author_id):
+    # TODO: Input check.
+    rq = requests.get(DATABASE_REST_URL
+                      + f'/author?ident=eq.{author_id}').json()
+    return jsonify(rq)
+
+
+@app.route(f'/api/v{__version__}/publication/<publication_id>',
+           methods=["GET"])
+def get_publication(publication_id):
+    # TODO: Input check.
+    rq = requests.get(DATABASE_REST_URL
+                      + f'/publication?ident=eq.{publication_id}').json()
+    return jsonify(rq)
