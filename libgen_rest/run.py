32a5d0b1c264fccba89ad9fd55fc6294ec22509d
Ahmet Can Küçükkör <ackucukkor@dijibt.com>
2020-02-28 18:57:22 +0300

    ADD: Libgen Rest Service

---
+++ libgen_rest/run.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""Run application using `Gunicorn`_.
+
+.. _Gunicorn:
+    http://docs.gunicorn.org/en/stable/
+"""
+
+import os
+
+
+if __name__ == '__main__':
+    expose_port = os.getenv("EXPOSE_PORT") or 5090
+    os.system(f"gunicorn application:app --worker-class eventlet -w 1 "
+              f"--bind 0.0.0.0:{expose_port} --reload --no-sendfile "
+              f"--timeout 120")
